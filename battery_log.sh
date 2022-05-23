d4807b81ca026f489abf8f02e0791719d98796ee
Nightfeather <ss900328@gmail.com>
2022-05-23 23:11:57 +0800

    Allow specify different battery

--- battery_log.sh
+++ battery_log.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+if [[ $# -lt 1 ]]; then
+  echo "
+  Usage: $0 <battery name>
+"
+  exit
+fi
+
+NAME=${1:-"BAT0"}
+
+BASE_PATH="/sys/class/power_supply/$NAME/"
+
 [[ -a /etc/default/battery_log ]] && source /etc/default/battery_log
 
 DATABASE_DIR=${DATABASE_DIR:-/var/lib}/battery_log
 
 [[ -d ${DATABASE_DIR} ]] || mkdir $DATABASE_DIR
 
-charge_now=$(cat /sys/class/power_supply/BAT1/charge_now)
-charge_full=$(cat /sys/class/power_supply/BAT1/charge_full)
+charge_now=$(cat $BASE_PATH"charge_now")
+charge_full=$(cat $BASE_PATH"charge_full")
 
 echo $(date "+%s") $charge_now $charge_full >> ${DATABASE_DIR}/history
 
